16cbeb646f07e4430b16c1ea2fe743bd5a0b9556
Chenilyn Joy Espineda <chlynespined@gmail.com>
2025-12-13 17:16:13 -0500

    UI Verification for Company Overview

--- cypress/e2e/company/overview.cy.js
+++ cypress/e2e/company/overview.cy.js
@@ -1,1 +1,35 @@
 // COMPANY OVERVIEW E2E TEST
+
+// -------------------------------------------------------------------------------------------------------------------- 
+// UI VERIFICATION 
+// Checking if all UI elements are visible upon page entry
+
+describe('UI Verification', () => {
+
+  // Logging in user before each test
+  beforeEach(() => {
+
+    cy.login();
+
+  });
+
+  it('displays overview infornation', () => {
+
+    // Verifying header is present
+    cy.contains(/Company Overview/i).should('be.visible');
+
+    // Verifying key company account information are present
+    cy.contains(/Total Members/i).should('be.visible');
+    cy.contains(/Workspaces/i).should('be.visible');
+    cy.contains(/Subscription Tier/i).should('be.visible');
+
+    // Verifying main company credentials are present
+    cy.fixture('company').then((company) => {
+      cy.contains(company.name).should('be.visible');
+      cy.contains(company.email).should('be.visible');
+      cy.contains(company.website).should('be.visible');
+    });
+
+  });
+
+});
