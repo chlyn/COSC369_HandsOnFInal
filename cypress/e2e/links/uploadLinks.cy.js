3428525525c7c36dc9701c305bc9af7dafd935a8
Chenilyn Joy Espineda <chlynespined@gmail.com>
2025-12-14 15:36:08 -0500

    Create Link Success Scenario

--- cypress/e2e/links/uploadLinks.cy.js
+++ cypress/e2e/links/uploadLinks.cy.js
@@ -30,3 +30,62 @@ describe('UI Verification', () => {
   });
 
 });
+
+
+
+// -------------------------------------------------------------------------------------------------------------------- 
+// SUCCESS SCENARIOS 
+// Verifying that users can create a new link
+
+describe('Success Scenarios', () => {
+
+  // Logging in user before each test
+  beforeEach(() => {
+
+    cy.login('user_1');
+    cy.get('button[title="Quick Apps"]').click();
+    cy.contains(/Quick Links/i).click();
+
+  });
+
+  // Scenario where user creates a new liink
+  it('create link', () => {
+
+    // Monitoring the backend API request and response
+    cy.intercept('POST', '**api/v1/web/upload-links').as('createLink');
+
+    // Creating new link by clicking "New Link" button
+    cy.contains('button', /New Link/i).click();
+
+    // Verifying create link form is present
+    cy.contains(/Create Upload Link/i).should('be.visible');
+
+    // Filling out create link form and submitting by clicking "Create Link" button 
+    cy.get('input[placeholder="Enter link name"]').type('Testing Link');
+    cy.contains('button', /Create Link/i).click();
+
+    // Verifying that the backend receives and responds correctly
+    cy.wait('@createLink').then(({request, response}) => {
+
+      // Verifying the correct API endpoint and HTTP method was used for the request
+      expect(request.url).to.include('api/v1/web/upload-links');
+      expect(request.method).to.eq('POST');
+      expect(request.body).to.include({
+        link_name: 'Testing Link'
+      });
+
+      // Verifying that the backend responded successfully
+      expect(response.statusCode).to.eq(200);
+      expect(response.body).to.include({
+        message: 'Upload link created successfully',
+        success: true
+      });
+
+    });
+
+    // Verifying that success message is present in the UI
+    cy.contains(/Upload link created successfully/i).should('be.visible').highlight();
+
+  });
+
+});
