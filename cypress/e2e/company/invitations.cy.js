ccdbafbe30bfd05a894fbc70853f88804b76557e
Chenilyn Joy Espineda <chlynespined@gmail.com>
2025-12-14 13:19:08 -0500

    UI Verification for Company Invitations

--- cypress/e2e/company/invitations.cy.js
+++ cypress/e2e/company/invitations.cy.js
@@ -1,1 +1,25 @@
 // COMPANY INVITATIONS E2E TEST
+
+// -------------------------------------------------------------------------------------------------------------------- 
+// UI VERIFICATION 
+// Checking if all UI elements are visible upon page entry
+
+describe('UI Verification', () => {
+
+  // Logging in user before each test
+  beforeEach(() => {
+
+    cy.login('user_1');
+    cy.contains('button', /Invitations/i).click();
+
+  });
+
+  // Verifying that all page elements are rendered into the page
+  it('displays invitations information', () => {
+
+    // Verifying header is present
+    cy.contains(/Pending Invitations/i).should('be.visible').highlight();
+
+  });
+
+});
