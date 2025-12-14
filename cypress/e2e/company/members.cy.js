f7cd4ebfa25b71e56f04096b6aaab3a267378d00
Chenilyn Joy Espineda <chlynespined@gmail.com>
2025-12-13 19:31:02 -0500

    Inviting a Member Success Scenario

--- cypress/e2e/company/members.cy.js
+++ cypress/e2e/company/members.cy.js
@@ -23,7 +23,7 @@ describe('UI Verification', () => {
     cy.contains('button', /Invite Member/i).should('be.visible');
     cy.get('input[type="email"]').should('be.visible');
     cy.get('select').contains('option', 'Member');
-    cy.contains('button', /Invite/i).should('be.visible');
+    cy.contains('button', /^Invite$/i).should('be.visible');
 
     // Verifying that admin is present in the member list
     cy.fixture('user').then((user) => {
@@ -32,6 +32,65 @@ describe('UI Verification', () => {
       cy.contains(user.lastName).should('be.visible');
       cy.contains(user.email).should('be.visible');
     });
+
+  });
+
+});
+
+
+
+// -------------------------------------------------------------------------------------------------------------------- 
+// SUCCESS SCENARIOS 
+// Verifying that members can invite other members and are listed
+
+describe('Success Scenarios', () => {
+
+  // Logging in user before each test
+  beforeEach(() => {
+
+    cy.login();
+    cy.contains('button', /Members/i).click();
+
+  });
+
+  it('invite new members', () => {
+
+    // Monitoring the backend API request and response
+    cy.intercept('POST', '**api/v1/company/3/invite').as('inviteMembers');
+
+    // Generating a unique email to invite
+    const unique = Date.now().toString();
+    const email = unique + '@example.com';
+
+    // Filling out email information
+    cy.get('input[type="email"]').type(email);
+
+    // Submiting the member invite by selecting "Invite"
+    cy.contains('button', /^Invite$/i).click();
+
+    // Verifying that the backend receives and responds correctly
+    cy.wait('@inviteMembers').then(({request, response}) => {
+
+      // Verifying the correct API endpoint, HTTP method, email, and username was used for the request
+      expect(request.url).to.include('/api/v1/company/3/invite');
+      expect(request.method).to.eq('POST');
+      expect(request.body).to.include({
+        email: email,
+        role: 'member'
+      });
+
+      // Verifying that the backend responded successfully
+      expect(response.statusCode).to.eq(200);
+      expect(response.body).to.include.keys("invitation", "message", "success");
+      expect(response.body).to.include({
+        message: 'Invitation created and sent successfully',
+        success: true
+      });
+
+    });
+
+    // Verifying that success message is present in the UI
+    cy.contains(`Invitation sent to ${email}`).should('be.visible');
 
   });
 
