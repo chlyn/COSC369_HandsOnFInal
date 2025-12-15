aef300ec78255850d1eca672abd2ac44904a32b6
Chenilyn Joy Espineda <chlynespined@gmail.com>
2025-12-14 19:07:32 -0500

    UI Verification for Quick Links Uploading A File

--- cypress/e2e/links/uploadingFile.cy.js
+++ cypress/e2e/links/uploadingFile.cy.js
@@ -1,5 +1,31 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+// UPLOADING A FILE E2E TEST
+
+// -------------------------------------------------------------------------------------------------------------------- 
+// UI VERIFICATION 
+// Checking if all UI elements are visible upon page entry
+
+describe('UI Verification', () => {
+
+  // Logging in user before each test
+  beforeEach(() => {
+
+    cy.visit('/upload-to/toQTa4EKmhSbTlj-AmFyTu-sXmhKZCY2aHSmfhreQ98');
+  
+  });
+
+  it('display upload file page', () => {
+
+    // Verifying header is present
+    cy.contains(/Upload Files/i).should('be.visible').highlight();
+
+    // Verifying  buttons are present for upload links tab
+    cy.contains(/Drop files here or click to browse/i).should('be.visible').highlight();
+    cy.get('input[placeholder="Enter your name"]').should('be.visible').highlight();
+    cy.get('input[type="email"]').should('be.visible').highlight();
+    cy.get('textarea[placeholder="Add a note about these files..."]').should('be.visible').highlight();
+    cy.get('input[placeholder="Enter action code if provided"]').should('be.visible').highlight();
+    cy.contains('button', /Upload 0 Files/i).should('be.visible').and('be.disabled').highlight();
+
+  });
+
+});
